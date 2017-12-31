58c1f24a66610980bfb0bea6d2a4cb0a5847a503
Bartek Kozera <bartosz.kozera@gmail.com>
2017-12-31 10:37:38 +0100

    add umd

--- sudoku.js
+++ sudoku.js
@@ -7,8 +7,21 @@
     Please see the README for more details.
 */
 
-(function(root){
-    var sudoku = root.sudoku = {};  // Global reference to the sudoku library
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.sudoku = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
+    var sudoku = {}; 
 
     sudoku.DIGITS = "123456789";    // Allowed sudoku.DIGITS
     var ROWS = "ABCDEFGHI";         // Row lables
@@ -803,5 +816,6 @@
     // Initialize library after load
     initialize();
 
-// Pass whatever the root object is, lsike 'window' in browsers
-})(this);
+    return sudoku;
+}));
+
